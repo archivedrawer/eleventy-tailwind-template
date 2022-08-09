4ce920094b4495700e2db3567e3aaa528aaf66f5
pedromchd <pedromchd@outlook.com.br>
2022-08-09 20:55:33 -0300

    passthrough config

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.ignores.add("*.md");
   eleventyConfig.setServerPassthroughCopyBehavior('copy');
+  eleventyConfig.addPassthroughCopy("assets");
 }
